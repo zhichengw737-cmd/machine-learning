a727498216391c3d706c1748a534626fc040af15
TommyCheung <tommycheung43@gmail.com>
2026-06-10 11:35:05 +0800

    Add route for supervised learning page
    Added a new route for the supervised learning page.

--- RL_SL_maze_web/machine learning/app.py
+++ RL_SL_maze_web/machine learning/app.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
+
+# route to handle supervised learning page
+@app.route('/supervised.html')
+def supervised():
+    return render_template('supervised.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
